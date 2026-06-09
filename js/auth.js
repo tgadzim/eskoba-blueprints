bf9521651ef9a33e1e8c001230b102499d601052
tgadzim <tengku.manager@gmail.com>
2026-06-10 03:38:10 +0800

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,28 @@
+import { auth } from "../firebase/config.js";
 
+import {
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js";
+
+window.loginUser = async function() {
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+
+    await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+
+    window.location.href = "home.html";
+
+  } catch(error) {
+
+    alert(error.message);
+
+  }
+
+}
